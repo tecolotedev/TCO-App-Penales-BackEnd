581eb400cc9559859b809fd8a4e679dba5202943
Manuel11713 <44284296+Manuel11713@users.noreply.github.com>
2019-08-01 10:40:11 -0500

    base de datos online

--- config/config.js
+++ config/config.js
@@ -12,12 +12,12 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev'; // dev == local
 //=====================
 //=====BASE DATOS======
 //=====================
-let urlDB='mongodb+srv://Manuel117:H6n667sgelXmw7qP@cluster0-rhrav.mongodb.net/test?retryWrites=true&w=majorityy';
-// if (process.env.NODE_ENV === 'dev') {
-//     urlDB = 'mongodb://localhost:27017/penalesDev'
-// } else {
-//     urlDB = 'mongodb+srv://Manuel11713:Spartan11713!!/!#qwerty@cluster0-rhrav.mongodb.net/test?retryWrites=true&w=majority'; // Variables de entorno personalizadas de Heroku o en produccion'
-// }
+let urlDB='';
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/penalesDev'
+} else {
+    urlDB = 'mongodb+srv://Manuel117:H6n667sgelXmw7qP@cluster0-rhrav.mongodb.net/test?retryWrites=true&w=majorityy'; // Variables de entorno personalizadas de Heroku o en produccion'
+}
  process.env.URLDB = urlDB;
 
 
