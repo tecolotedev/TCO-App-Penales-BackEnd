805391a499df6e121721410719fcd4d013d63f10
Manuel11713 <44284296+Manuel11713@users.noreply.github.com>
2019-08-01 10:39:20 -0500

    Se agrego la base datos

--- config/config.js
+++ config/config.js
@@ -12,13 +12,13 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev'; // dev == local
 //=====================
 //=====BASE DATOS======
 //=====================
-let urlDB='';
-if (process.env.NODE_ENV === 'dev') {
-    urlDB = 'mongodb://localhost:27017/penalesDev'
-} else {
-    urlDB = 'mongodb+srv://Manuel11713:Spartan11713!!/!#qwerty@cluster0-rhrav.mongodb.net/test?retryWrites=true&w=majority'; // Variables de entorno personalizadas de Heroku o en produccion'
-}
-process.env.URLDB = urlDB;
+let urlDB='mongodb+srv://Manuel117:H6n667sgelXmw7qP@cluster0-rhrav.mongodb.net/test?retryWrites=true&w=majorityy';
+// if (process.env.NODE_ENV === 'dev') {
+//     urlDB = 'mongodb://localhost:27017/penalesDev'
+// } else {
+//     urlDB = 'mongodb+srv://Manuel11713:Spartan11713!!/!#qwerty@cluster0-rhrav.mongodb.net/test?retryWrites=true&w=majority'; // Variables de entorno personalizadas de Heroku o en produccion'
+// }
+ process.env.URLDB = urlDB;
 
 
 //=====================
